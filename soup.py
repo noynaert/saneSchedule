0e39d8e8523ea57aa75db7e180f437d8035db733
MesminN <70533175+MesminN@users.noreply.github.com>
2020-10-29 09:42:47 -0500

    format the csv file: add a header

--- soup.py
+++ soup.py
@@ -18,6 +18,12 @@ def set_base_url(year, semester):
     
     return f"https://aps4.missouriwestern.edu/schedule/Default.asp?tck={year}{semester_code}"
 
+def remove_field_name(expression, field_name):
+    if(expression.startswith(field_name)):
+        expression = expression.replace(field_name, '')
+        expression = expression.strip()
+    return expression
+
 base_url = set_base_url(2021, "fall")
 html_file = "post_request_file.html"
 
@@ -62,6 +68,11 @@ for td_item in soup.find_all('td', class_='detail_cell'):
     if len(detail_cell_list) == 8:
         detail_cell_list.remove('Full') 
     for detail in detail_cell_list:
+        detail = remove_field_name(detail, 'Maximum Enrollment:')
+        detail = remove_field_name(detail, 'Section Seats Available:')
+        detail = remove_field_name(detail, 'ADDITIONAL FEE:')
+        detail = remove_field_name(detail, 'Course Begins:')
+        detail = remove_field_name(detail, 'Course Ends:')
         courses[i][j] = detail
         print(i, j, detail)
         j += 1
@@ -70,12 +81,27 @@ for td_item in soup.find_all('td', class_='detail_cell'):
     j = 10
     print("\n")
 
+
+field_names = ['CRN', 'Course', 'Sec', 'Type', 'Title', 'Hrs', 'Days', 'Times', 'Room', 'Instructor', 'Maximum Enrollment', 'Section Seats Available', 'Course Details', 'ADDITIONAL FEE', 'Special Note', 'Course Begins', 'Course Ends']
 with open('data_csv.csv', 'w+', newline='') as csv_file:
-    csv_writer = csv.writer(csv_file)
+    csv_writer = csv.DictWriter(csv_file, fieldnames = field_names)
+    csv_writer.writeheader()
 
     #Write courses values in a csv file
-    for idx in range(1, len(courses)):
-        csv_writer.writerow(courses[idx])
+    for row_idx in range(0, len(courses)):
+
+        #Start by removing special char
+        for column_idx in range(0, len(courses[row_idx])):
+            field_value = courses[row_idx][column_idx].replace('\t', '')
+            field_value = field_value.replace('\xa0', '')
+            courses[row_idx][column_idx] = field_value
+            
+        csv_writer.writerow({field_names[0]:courses[row_idx][0], field_names[1]:courses[row_idx][1],field_names[2]:courses[row_idx][2],
+                            field_names[3]:courses[row_idx][3], field_names[4]:courses[row_idx][4], field_names[5]:courses[row_idx][5],
+                            field_names[6]:courses[row_idx][6], field_names[7]:courses[row_idx][7], field_names[8]:courses[row_idx][8],
+                            field_names[9]:courses[row_idx][9], field_names[10]:courses[row_idx][10], field_names[11]:courses[row_idx][11],
+                            field_names[12]:courses[row_idx][12],field_names[13]:courses[row_idx][13],field_names[14]:courses[row_idx][14],
+                            field_names[15]:courses[row_idx][15],field_names[16]:courses[row_idx][16]})
 
 csv_file.close()
 html_file_to_parse.close()
